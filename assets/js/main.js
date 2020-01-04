dee5c76949ea4d6632c5b2956366726998c95d64
encisochristian <encisochristianmalana@gmail.com>
2020-01-04 23:17:34 +0800

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,3 @@
-// var menuIcon = new Image();
-// menuIcon.id = "menuIcon";
-// menuIcon.src = "assets/images/Mobile/1.png";
-
-// var closeIcon = document.createElement("span");
-// closeIcon.className = "close-icon";
-// closeIcon.innerHTML = "&#10006;";
-
-// var toggleIconContainer = document.getElementById("toggle");
-
-// var menuItems = document.querySelectorAll(".menu-item");
-// var menuKeyArray = Object.keys(menuItems);
-
-// function onToggleMobileMenu() {
-//   var isMenuOpen = toggleIconContainer.contains(closeIcon);
-//   if (isMenuOpen) {
-//     menuKeyArray.forEach(function(key) {
-//       menuItems[key].style.display = "none";
-//     });
-//     closeIcon.replaceWith(menuIcon);
-//   } else {
-// menuKeyArray.forEach(function(key) {
-//   menuItems[key].style.display = "block";
-// });
-//     menuIcon.replaceWith(closeIcon);
-//   }
-// }
-
-// function initToggleIcon() {
-//   toggleIconContainer.appendChild(menuIcon);
-// }
-
-// window.addEventListener("load", initToggleIcon());
-
 var menuItems = document.querySelectorAll(".menu-item");
 var menuKeyArray = Object.keys(menuItems);
 var overlayMenuTitle = document.querySelector("#mobile-menu p.overlay-title");
@@ -49,7 +15,7 @@ function changeOverlayMenuTitle(newTitle) {
 }
 
 function initOverlayMenuContent() {
-  var overlayMenu = document.querySelector("#mobile-menu ul.menu");
+  var overlayMenu = document.querySelector("#mobile-menu ul");
   menuKeyArray.forEach(function(key) {
     var clone = menuItems[key].cloneNode(true);
     overlayMenu.appendChild(clone);
